1af15097acb423f9c7458a74ab9f252dd8af2173
CHEOLJIN <kcj091857@gmail.com>
2017-07-14 00:06:57 +0900

    [DI]spring-test

---
+++ src/myspring/di/xml/test/HelloBeanJunitSpringTest.java
@@ -0,0 +1,47 @@
+package myspring.di.xml.test;
+
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import myspring.di.xml.Hello;
+import myspring.di.xml.Printer;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="classpath:config/beans.xml")
+public class HelloBeanJunitSpringTest {
+	@Autowired
+	ApplicationContext context;
+	
+	@Test 
+	@Ignore
+	public void test1() {
+		//2. getBean() 호출
+		Hello hello = (Hello)context.getBean("hello");
+		//3. Hello의 sayHello() 호출
+		assertEquals("Hello Spring", hello.sayHello());
+		//3. Hello의 printer() 호출
+		hello.print();
+		
+		//StringPrinter getBean()
+		Printer printer = context.getBean("printer", Printer.class);
+		assertEquals("Hello Spring", printer.toString());
+	}
+	
+	@Test
+	public void test2() {
+		//2. getBean() 호출
+		Hello hello = (Hello)context.getBean("hello");
+		
+		Hello hello2 = (Hello)context.getBean("hello");
+		
+		assertSame(hello, hello2);
+	}
+
+}
