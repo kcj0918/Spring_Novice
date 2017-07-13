46c5440b88a2735e99e8645359935c61b74887e7
CHEOLJIN <kcj091857@gmail.com>
2017-07-14 00:23:01 +0900

    [DI]collection type 값 주입

--- src/myspring/di/xml/test/HelloBeanJunitSpringTest.java
+++ src/myspring/di/xml/test/HelloBeanJunitSpringTest.java
@@ -13,6 +13,8 @@ import myspring.di.xml.Printer;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations="classpath:config/beans.xml")
 public class HelloBeanJunitSpringTest {
@@ -20,21 +22,26 @@ public class HelloBeanJunitSpringTest {
 	ApplicationContext context;
 	
 	@Test 
-	@Ignore
 	public void test1() {
 		//2. getBean() 호출
-		Hello hello = (Hello)context.getBean("hello");
+		Hello hello = (Hello)context.getBean("hello2");
 		//3. Hello의 sayHello() 호출
 		assertEquals("Hello Spring", hello.sayHello());
 		//3. Hello의 printer() 호출
 		hello.print();
+		
+		assertEquals(3, hello.getNames().size());
+		List<String> list = hello.getNames();
+		for(String value : list) {
+			System.out.println(value);
+		}
 		
 		//StringPrinter getBean()
 		Printer printer = context.getBean("printer", Printer.class);
 		assertEquals("Hello Spring", printer.toString());
 	}
 	
-	@Test
+	@Test @Ignore
 	public void test2() {
 		//2. getBean() 호출
 		Hello hello = (Hello)context.getBean("hello");
